90caeff389360859cf461b9740146e4999062c5c
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-09-10 10:58:31 +0530

    Configured gitignore

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: ".vars/config.env" });
 const app = require("./app");
 
 const DB = process.env.DATABASE;
